8554dad0876fc7f1b0f341919759db5a2616cfc0
danielagustinllanos <llanosdanielagustin@gmail.com>
2023-08-23 10:01:41 -0300

    commit

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/widget/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widget/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widget/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -24,9 +25,7 @@ class ChatScreen extends StatelessWidget {
 }
 
 class _ChatView extends StatelessWidget {
-  const _ChatView({
-    super.key,
-  });
+  const _ChatView();
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +41,7 @@ class _ChatView extends StatelessWidget {
                 return (index % 2 == 0) ? const HerMessageBubble() : const MyMessageBubble();
               },
             )),
-            Text('data'),
+            const MessageFieldBox(),
           ],
         ),
       ),
--- lib/presentation/widget/chat/her_message_bubble.dart
+++ lib/presentation/widget/chat/her_message_bubble.dart
@@ -23,12 +23,38 @@ class HerMessageBubble extends StatelessWidget {
             ),
           ),
         ),
-        const SizedBox(
-          height: 5,
-
-          // TODO: IMAGEN
-        )
+        const SizedBox(height: 5),
+        const _ImageBubble(),
+        const SizedBox(height: 10),
       ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  const _ImageBubble();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        'https://yesno.wtf/assets/yes/0-c44a7789d54cbdcad867fb7845ff03ae.gif',
+        width: size.width * 0.7,
+        height: 150,
+        fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text('enviando imagen'),
+          );
+        },
+      ),
     );
   }
 }
